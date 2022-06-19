7081b32749ebed52ffdd2fb85734fae0a7190e4f
lipe-pepe <felipepepe21@gmail.com>
2022-06-19 14:32:47 -0300

    Added the frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { MdLogin } from "react-icons/md";
+
+function App() {
+  return (
+    <div className="App">
+      <div className='login'>
+      <MdLogin className='icon'></MdLogin>
+      </div>
+    </div>
+  );
+}
+
+export default App;
